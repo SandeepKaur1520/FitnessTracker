d38f9cb5c9183bfa99dbd1a3c332a1d9468b69b1
sandeepkaur1520 <deepdharamsot1520@gmail.com>
2019-06-25 23:55:14 +0530

    next button done in login screen

---
+++ app/src/main/java/com/fitness/tracker/Personal_info.java
@@ -0,0 +1,14 @@
+package com.fitness.tracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Personal_info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.personal_info);
+    }
+}
