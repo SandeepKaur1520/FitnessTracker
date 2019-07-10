724b51cd958f8be5bece8f07bfda5a396041dc90
sandeepkaur1520 <deepdharamsot1520@gmail.com>
2019-07-11 03:18:25 +0530

    gym dashboard is going on

--- app/src/main/java/com/fitness/tracker/BodyActivity.java
+++ app/src/main/java/com/fitness/tracker/BodyActivity.java
@@ -13,13 +13,13 @@ public class BodyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bodyactivity);
-        /*Button nextBtn = findViewById(R.id.nextBtn);
+
+        Button nextBtn = findViewById(R.id.bNextBodyActivity);
         nextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(BodyActivity.this,stepsChart.class));
+                startActivity(new Intent(BodyActivity.this,GymTrainer_Dashboard.class));
             }
         });
-    }*/
     }
 }
---
+++ app/src/main/java/com/fitness/tracker/gymfragment/GymHomepageFragment.java
@@ -0,0 +1,29 @@
+package com.fitness.tracker.gymfragment;
+
+
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.fitness.tracker.R;
+
+
+public class GymHomepageFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view =inflater.inflate(R.layout.homepagefragment,container,false);
+        return view;
+    }
+
+    public interface OnFragmentInteractionListener {
+        void onFragmentInteraction(Uri uri);
+    }
+}
