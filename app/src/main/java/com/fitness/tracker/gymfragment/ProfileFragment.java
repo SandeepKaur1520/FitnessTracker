dbeffedbe3b24459bb3d89aa8c011797517c0aff
Diljot Singh <diljots99@gmail.com>
2019-07-13 20:47:46 +0530

    Health Record UI Design

---
+++ app/src/main/java/com/fitness/tracker/gymfragment/ProfileFragment.java
@@ -0,0 +1,26 @@
+package com.fitness.tracker.gymfragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.fitness.tracker.R;
+
+
+public class ProfileFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+
+
+    }
+}
