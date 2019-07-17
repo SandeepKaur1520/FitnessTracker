3897d8f4ffcfcbbc47e56dc41af9b08438a1e135
Diljot Singh <diljots99@gmail.com>
2019-07-18 01:56:46 +0530

    Kaaffi kuch kar ta fher to yaad ni

---
+++ app/src/main/java/com/fitnesstracker/Daily_Goals.java
@@ -0,0 +1,13 @@
+package com.fitnesstracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Daily_Goals extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.daily__goals);
+    }
+}
---
+++ app/src/main/java/com/fitnesstracker/database/getCurrentDate.java
@@ -0,0 +1,12 @@
+package com.fitnesstracker.database;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class getCurrentDate {
+    public  int[] get(){
+
+        return  null;
+    }
+
+}
