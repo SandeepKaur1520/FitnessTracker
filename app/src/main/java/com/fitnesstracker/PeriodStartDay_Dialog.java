3f3ed3978fe5d8d8948ea03706a2442932cec438
Diljot Singh <diljots99@gmail.com>
2019-07-17 15:02:48 +0530

    period dialog is pending

---
+++ app/src/main/java/com/fitnesstracker/PeriodStartDay_Dialog.java
@@ -0,0 +1,13 @@
+package com.fitnesstracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PeriodStartDay_Dialog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.period_start_day__dialog);
+    }
+}
