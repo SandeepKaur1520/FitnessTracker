64b073891b850d85d52cdc8a3c19639896cbbad5
sandeepkaur1520 <deepdharamsot1520@gmail.com>
2019-07-18 15:59:44 +0530

    personal info valid

--- app/src/main/java/com/fitnesstracker/Personal_info.java
+++ app/src/main/java/com/fitnesstracker/Personal_info.java
@@ -3,30 +3,29 @@ package com.fitnesstracker;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.fitnesstracker.database.DatabaseHelper;
 
 public class Personal_info extends AppCompatActivity {
 
-    EditText  firstName ;
-    EditText lastName;
+    EditText  firstName,lastName,email,pass, cnfPass ;;
     Spinner gender;
-    EditText email ;
-    EditText pass ;
-    EditText cnfPass ;
-
-    String Gender;
-    String FirstName ;
-    String LastName;
-    String eMail;
-    String Pass;
-    String CnfPass;
+    TextView invalidEmailMessage,invalidConfirmPass;
+    Boolean isEmailValid,isConfirmPassValid;
+    String Gender, FirstName, LastName, eMail, Pass, CnfPass;
 
 
     @Override
@@ -40,7 +39,8 @@ public class Personal_info extends AppCompatActivity {
         email = findViewById(R.id.etemail);
         pass = findViewById(R.id.etPass);
         cnfPass = findViewById(R.id.etconfirm_password);
-
+        invalidEmailMessage =findViewById(R.id.tvInvalidEmailMessage);
+        Gender = "Male";
 
 
 
@@ -57,9 +57,7 @@ public class Personal_info extends AppCompatActivity {
                 else if(x.equals("Female")){
                     Gender = "Female";
                 }
-                else if(x.equals("Other")){   /*gender.getSelectedItem().toString()*/
-                    Gender = "Other";
-                }
+
             }
 
             @Override
@@ -67,11 +65,52 @@ public class Personal_info extends AppCompatActivity {
 
             }
         });
+        email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+            }
 
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if((!TextUtils.isEmpty(s)) && (Patterns.EMAIL_ADDRESS.matcher(s).matches()) ){//when email is valid
+                    invalidEmailMessage.setVisibility(View.GONE);
+                    isEmailValid=true;
+                }else{
+                    isEmailValid=false;
+                    invalidEmailMessage.setVisibility(View.VISIBLE);
+
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        cnfPass.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String password = pass.getText().toString();
+//                if((!TextUtils.isEmpty(s)) && (password.matches(s))){
+//
+//                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
         Button next = findViewById(R.id.bNext);
-
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -81,21 +120,28 @@ public class Personal_info extends AppCompatActivity {
                 eMail = email.getText().toString();
                 Pass = pass.getText().toString();
                 CnfPass = cnfPass.getText().toString();
-                boolean status = false;
-                try{
-                    status = db.createUser(eMail, Pass, FirstName, LastName, Gender);
-                }catch (Exception e){
-                    Toast.makeText(Personal_info.this,"Email Already Exists",Toast.LENGTH_LONG).show();
-                }
+                if((isEmailValid) ) {
+                    invalidEmailMessage.setVisibility(View.GONE);
 
-                if(status) {
-                    Toast.makeText(Personal_info.this,"Data Saved",Toast.LENGTH_LONG).show();
-                    Intent intent = new Intent(Personal_info.this, Health_Info.class);
-                    intent.putExtra("email",eMail);
-                    startActivity(intent);
-                }
-                else{
-                    Toast.makeText(Personal_info.this,"Something Went Wrong",Toast.LENGTH_LONG).show();
+                    boolean status = false;
+                    try {
+                        status = db.createUser(eMail, Pass, FirstName, LastName, Gender);
+                    } catch (Exception e) {
+                        Toast.makeText(Personal_info.this, "Email Already Exists", Toast.LENGTH_LONG).show();
+                    }
+
+                    if (status) {
+                        Toast.makeText(Personal_info.this, "Data Saved", Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(Personal_info.this, Health_Info.class);
+                        intent.putExtra("email", eMail);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(Personal_info.this, "Something Went Wrong", Toast.LENGTH_LONG).show();
+                    }
+
+                }else {
+                    invalidEmailMessage.setVisibility(View.VISIBLE);
+                    Toast.makeText(Personal_info.this,"Please Enter a valid email",Toast.LENGTH_LONG).show();
                 }
             }
         });
