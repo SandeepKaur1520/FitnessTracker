ccdb306fd40ee82c06bbab1016b9b9b0ac21fbf8
Diljot Singh <diljots99@gmail.com>
2019-07-19 11:49:06 +0530

    nothing special

---
+++ app/src/main/java/com/fitnesstracker/Gym_CongratsActivity.java
@@ -0,0 +1,13 @@
+package com.fitnesstracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Gym_CongratsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gym__congrats);
+    }
+}
