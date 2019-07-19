531b34919009d105c50f50c4bc0188f3d04077c6
Diljot Singh <diljots99@gmail.com>
2019-07-19 19:33:42 +0530

    nothing special is done

--- app/src/main/java/com/fitnesstracker/BodyActivity.java
+++ app/src/main/java/com/fitnesstracker/BodyActivity.java
@@ -2,6 +2,7 @@ package com.fitnesstracker;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -24,12 +25,12 @@ public class BodyActivity extends AppCompatActivity {
         RGbodyActiveness.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                if(checkedId==R.id.rbtndWorkoutHero){
-                    BodyActiveness="Workout Hero";
-                }else if(checkedId==R.id.rbtnlightMode){
-                    BodyActiveness="Light Mode";
-                }else if(checkedId==R.id.rbtnNewOne){
-                    BodyActiveness="New One";
+                if (checkedId == R.id.rbtndWorkoutHero) {
+                    BodyActiveness = "Workout Hero";
+                } else if (checkedId == R.id.rbtnlightMode) {
+                    BodyActiveness = "Light Mode";
+                } else if (checkedId == R.id.rbtnNewOne) {
+                    BodyActiveness = "New One";
                 }
 
             }
@@ -37,16 +38,33 @@ public class BodyActivity extends AppCompatActivity {
         nextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Boolean status =false;
-                status = db.updateBodyActiveness(email,BodyActiveness);
-                if(status) {
+                Boolean status = false;
+                status = db.updateBodyActiveness(email, BodyActiveness);
+                if (status) {
                     Intent intent = new Intent(BodyActivity.this, GymTrainer_Dashboard.class);
                     intent.putExtra("email", email);
                     startActivity(intent);
-                }else{
-                    Toast.makeText(BodyActivity.this,"Something Went Wrong",Toast.LENGTH_LONG).show();
-                }
+                    Toast.makeText(BodyActivity.this,"Your Data Saved",Toast.LENGTH_LONG).show();
 
+                }
+                else {
+                    Toast.makeText(BodyActivity.this, "Something Went Wrong", Toast.LENGTH_LONG).show();
+                }
+                /*else{
+                    Toast.makeText(BodyInfo.this,"Please select at least one Option",Toast.LENGTH_LONG).show();
+                    CountDownTimer countDownTimer =new CountDownTimer(2000,20) {
+                        @Override
+                        public void onTick(long millisUntilFinished) {
+
+                        }
+
+                        @Override
+                        public void onFinish() {
+                            Toast.makeText(BodyInfo.this,"Please select at least one Option",Toast.LENGTH_LONG).show();
+                        }
+                    };
+                }
+*/
             }
         });
     }
